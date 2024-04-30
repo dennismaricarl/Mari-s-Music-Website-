a0d7e3007e81c332d13614142ae9c7e2a2baa7b5
dennismaricarl <dennismaricarl@gmail.com>
2024-04-30 14:01:38 -0700

    added videos on video page

--- music-website/src/components/Videos.jsx
+++ music-website/src/components/Videos.jsx
@@ -1,7 +1,32 @@
+import React from "react";
+import YouTube from 'react-youtube'
+
 const Videos = () => {
     return (
         <>
-        <h1>Videos Page</h1>
+            <div className="videos-container">
+                <YouTube className="video"
+                    videoId='GF8ReFkWCso'
+                />
+                <YouTube className="video"
+                    videoId="EAZU0U13QmA"
+                />
+                <YouTube className="video"
+                    videoId="Zmcyp5AQOCs"
+                />
+                <YouTube className="video"
+                    videoId="hdymdwJJePA"
+                />
+                <YouTube className="video"
+                    videoId="0f33cKjDDTg"
+                />
+                <YouTube className="video"
+                    videoId="MGXc2NFojos"
+                />
+                <YouTube className="video"
+                    videoId="4GMXEzagHrI"
+                />
+            </div>
         </>
     )
 }
