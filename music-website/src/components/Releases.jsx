ecfa9dfbc1584aade513a0478e1e20cb79e93aba
dennismaricarl <dennismaricarl@gmail.com>
2024-05-01 15:23:05 -0700

    releases page in progress

--- music-website/src/components/Releases.jsx
+++ music-website/src/components/Releases.jsx
@@ -1,8 +1,88 @@
-const Releases = () => {
-    return(
-        <>
-        <h1>RELEASES</h1>
-        </>
+import { React, useState } from 'react';
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import releasesData from '../releasesData';
+
+const ExpandMore = styled((props) => {
+    const { expand, ...other } = props;
+    return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+    transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest,
+    }),
+}));
+
+
+
+function Releases() {
+    const [expanded, setExpanded] = useState(false);
+    const [releases, setRelease] = useState([])
+
+    const handleExpandClick = (id) => {
+        const releases = releasesData.find((release) => release.id == id)
+        setRelease(releases.id)
+        if (releases){
+        setExpanded(!expanded)
+        }
+            
+         
+        }
+        
+
+
+    return (
+        <div style={{ margin: '40px', display: 'flex', justifyContent:'center'}}>
+            {releasesData.map((release) => (
+
+                <Card key={release.id} sx={{ maxWidth: 345, margin: 4}}>
+                    <CardMedia
+                        component="img"
+                        height="300"
+                        image={release.imageURL}
+                        alt="album cover image"
+                    />
+                    <CardContent>
+                    </CardContent>
+                    <CardActions disableSpacing>
+                        <IconButton aria-label="listen here">
+                            <FavoriteIcon />
+                        </IconButton>
+                        <ExpandMore
+                            expand={expanded}
+                            onClick={() => handleExpandClick(release.id)}
+                            aria-expanded={expanded}
+                            aria-label="show more"
+                        >
+                            <ExpandMoreIcon />
+                        </ExpandMore>
+                    </CardActions>
+                    <Collapse in={expanded} timeout="auto" unmountOnExit>
+                        <CardContent>
+                            <Typography fontFamily={'Georgia'}>
+                                {release.description}
+                            </Typography>
+                        </CardContent>
+                    </Collapse>
+                </Card>
+
+
+            ))}
+
+
+        </div>
+
+
     )
 }
 
---
+++ music-website/src/releasesData.js
@@ -0,0 +1,28 @@
+const releasesData = [
+    {   
+        id:1, 
+        imageURL: './images/holdMeAgain.png',
+        description: 
+        <p>My first EP is finally out now! This first EP project is a dream..It was no small feat as an indie artist, an amateur at most things and a really sappy human being lol. These songs not only reveal
+        my share of heartaches but they also are reflective of some of my deepest longings.I hope these songs allow you to feel deeply, however your heart takes each of them, and inspire you in some way. Thank you for listening and sharing. Thank you for giving my songs 
+        (and my heart really) a chance to be seen and heard. More to come!
+        Special thanks to the wonderful @haydenarpmusic for engineering and singing on the title track.'</p>
+    },
+    {   id:2,
+        imageURL: './images/theNextChapter.png',
+        description: 'Description HERE'
+    },
+    {   id:3,
+        imageURL: './images/holdMeAgain.png',
+        description: 'Description HERE'
+    },
+    {   id:4,
+        imageURL: './images/holdMeAgain.png',
+        description: 'Description HERE'
+    },
+
+
+
+]
+
+export default releasesData; 
