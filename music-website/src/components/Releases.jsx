dc6cd861e2b19e21fa003640064072563047df8d
dennismaricarl <dennismaricarl@gmail.com>
2024-05-12 21:55:34 -0700

    releases page updated

--- music-website/src/components/Releases.jsx
+++ music-website/src/components/Releases.jsx
@@ -1,59 +1,58 @@
 import { React, useState } from 'react';
-import { styled } from '@mui/material/styles';
 import Card from '@mui/material/Card';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
 import FavoriteIcon from '@mui/icons-material/Favorite';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import releasesData from '../releasesData';
 import PlayCircleOutlinedIcon from '@mui/icons-material/PlayCircleOutlined';
 import PauseCircleOutlineOutlinedIcon from '@mui/icons-material/PauseCircleOutlineOutlined';
 import ShareOutlinedIcon from '@mui/icons-material/ShareOutlined';
-
-
-
-
-const ExpandMore = styled((props) => {
-    const { expand, ...other } = props;
-    return <IconButton {...other} />;
-})(({ theme, expand }) => ({
-    transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-    marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
-        duration: theme.transitions.duration.shortest,
-    }),
-}));
+import TagIcon from '@mui/icons-material/Tag';
 
 
 
 function Releases() {
 
-    const [play, setPlay] = useState(false);
     const [audio, setAudio] = useState(null);
-    const [heartClicks, setHeartClicks] = useState([]);
+    const [heartClicks, setHeartClicks] = useState({});
+    const [card, setCardClicked] = useState({})
+    const [played, setPlayed] = useState({})
 
-    const playPause = (audioURL) => {
-        if (!play) {
+
+
+    const playPause = (id, audioURL) => {
+        setPlayed((playIDs) => {
+            const playList = { ...playIDs }
+            playList[id] = !playList[id]
+            return playList
+
+        })
+        if (!played[id]) {
             const newAudio = new Audio(audioURL);
             newAudio.play();
             setAudio(newAudio);
-            setPlay(true);
         } else {
             audio.pause();
-            setPlay(false);
+
         }
-    }
+    };
+
 
     const handleHeart = (id) => {
-        if(heartClicks.includes(id)) { // if id is already in array, heart has been clicked
-            setHeartClicks(heartClicks.filter((clickedId) => clickedId !== id)); //so remove it 
-        } else {
-            setHeartClicks([...heartClicks, id]); // if id not in array yet, heart hasn't been clicked, so include it!
-        }
+        setHeartClicks((hearts) => {
+            const heartsList = { ...hearts }
+            heartsList[id] = !heartsList[id]
+            return heartsList
+        })
+    };
+
+
+    const handleTag = (id) => {
+        setCardClicked((cards) => {
+            const cardsList = { ...cards }; // make a copy of current state to make sure we dont mutate the original state
+            cardsList[id] = !cardsList[id]; //toggle state of clicked card. set true to false and vice versa. 
+            return cardsList;  // returns updated cardList object 
+        });
     };
 
 
@@ -62,27 +61,35 @@ function Releases() {
             {releasesData.map((release, id) => (
 
                 <Card key={id} sx={{ maxWidth: 345, margin: 4 }}>
-                    <CardMedia
+
+                    <CardMedia 
                         component="img"
                         height="300"
                         image={release.imageURL}
                         alt="album cover image"
                     />
 
+
                     <CardContent>
                         <div style={{ display: 'flex' }}>
-                            <div onClick={() => playPause(release.audioURL)}>
-                                {play ?
-                                    <PauseCircleOutlineOutlinedIcon className="playPause" />
-                                    : <PlayCircleOutlinedIcon className="playPause" />}
+                            <div onClick={() => playPause(id, release.audioURL)}>
+                                {played[id] ?
+                                    <PauseCircleOutlineOutlinedIcon className="icons" />
+                                    : <PlayCircleOutlinedIcon className="icons" />}
                             </div>
-                            <ShareOutlinedIcon className="playPause" onClick={() => window.location.href = (release.linkfire)} />
-                            <FavoriteIcon 
-                            style={{padding: '4px'}}
-                            onClick={() => handleHeart(release.id)}
-                            className={heartClicks.includes(release.id) ? "redHeart" : "blackHeart"}
-                            
+                            <ShareOutlinedIcon className="icons" onClick={() => window.location.href = (release.linkfire)} />
+                            <FavoriteIcon
+                                style={{ padding: '4px' }}
+                                onClick={() => handleHeart(id)}
+                                className={heartClicks[id] ? "redHeart" : "blackHeart"}
+
                             />
+                            <button style={{ border: 'none', backgroundColor: 'white' }} onClick={() => handleTag(id)}>
+                                <TagIcon className='icons' />
+                                {card[id] ? (
+                                    <div style={{ display: 'flex', textAlign: 'left', fontFamily: 'Georgia' }}>{release.description}</div>) : null
+                                }
+                            </button>
                         </div>
                     </CardContent>
 
--- music-website/src/releasesData.js
+++ music-website/src/releasesData.js
@@ -1,22 +1,43 @@
 const releasesData = [
-    {   
-        id:1, 
+    {
+        id: 1,
         imageURL: './images/holdMeAgain.png',
-        description: 
-        <p>My first EP is finally out now! This first EP project is a dream..It was no small feat as an indie artist, an amateur at most things and a really sappy human being lol. These songs not only reveal
-        my share of heartaches but they also are reflective of some of my deepest longings.I hope these songs allow you to feel deeply, however your heart takes each of them, and inspire you in some way. Thank you for listening and sharing. Thank you for giving my songs 
-        (and my heart really) a chance to be seen and heard. More to come!
-        Special thanks to the wonderful @haydenarpmusic for engineering and singing on the title track.'</p>,
+        description:
+            <p>My first EP is finally out now! This first EP project is a dream..It was no small feat as an indie artist, an amateur at most things and a really sappy human being lol. These songs not only reveal
+                my share of heartaches but they also are reflective of some of my deepest longings.I hope these songs allow you to feel deeply, however your heart takes each of them, and inspire you in some way. Thank you for listening and sharing. Thank you for giving my songs
+                (and my heart really) a chance to be seen and heard. More to come!
+                Special thanks to the wonderful @haydenarpmusic for engineering and singing on the title track.'</p>,
         audioURL: './audio/HoldMeAgain.mp3',
+        linkfire: 'https://lnkfi.re/holdmeagain'
+    },
+    {
+        id: 2,
+        imageURL: './images/theNextChapter.png',
+        description: <p>✨ the next chapter ✨ OUT NOW!!!!!!! 🤍🤍
+            written/ produced by me. i tried okay. Haha
+            mix/mastered by favorite engineer, Sefi Carmel @soundtrackcreation ⚡️⚡️⚡️thank you Sefi!</p>,
+        audioURL: './audio/MovingOn.mp3',
         linkfire: 'https://lnkfi.re/thenextchapter'
     },
-    {   id:2,
-        imageURL: './images/theNextChapter.png',
-        description: ''
+    {
+        id: 3,
+        imageURL: './images/Daydreaming.png',
+        description: <p>This was the first song I ever released in 2018!🥺</p>,
+        linkfire: 'https://lnkfi.re/daydreamiing'
     },
-    {   id:3,
-        imageURL: './images/dayDreaming.png',
-        description: <p>This was the first song I released!</p>
+    {
+        id: 4,
+        imageURL: './images/idwlyd.png',
+        description: <p></p>,
+        audioURL: './audio/idwlyd.mp3',
+        linkfire: 'https://lnkfi.re/idwlyd'
+    },
+    {
+        id: 5,
+        imageURL: './images/alwaysYours.png',
+        description: <p>I’m happy to announce the re-release of “always yours” with @disrupcionrecords⚡️</p>,
+        audioURL: './audio/AlwaysYours.mp3',
+        // linkfire: 'https://lnkfi.re/idwlyd'
     }
 
 
